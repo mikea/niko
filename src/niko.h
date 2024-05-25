ecc731b5569c7ed11d5b30c8d77e481f57a9fd02
Mike Aizatsky <mike.aizatsky@gmail.com>
2024-05-25 11:35:09 -0700

    build type

--- src/niko.h
+++ src/niko.h
@@ -3,8 +3,13 @@
 #ifndef GIT_DESCRIBE
 #define GIT_DESCRIBE "unknown"
 #endif
+
 #ifndef COMPILE_TIME
 #define COMPILE_TIME "unknown"
 #endif
 
-#define VERSION_STRING "niko " GIT_DESCRIBE " (" COMPILE_TIME ") http://github.com/mikea/niko"
+#ifndef BUILD_TYPE
+#define BUILD_TYPE "unknown"
+#endif
+
+#define VERSION_STRING "niko " GIT_DESCRIBE " " BUILD_TYPE " (" COMPILE_TIME ") http://github.com/mikea/niko"
